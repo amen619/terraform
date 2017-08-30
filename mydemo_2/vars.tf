a6a569f9c4c3d3d1aa3a4c94496c8c043677844c
joudaon <jonuda77@gmail.com>
2017-08-30 17:13:46 +0200

    mydemo2 creates 2 instances with saltmaster software provision

---
+++ mydemo_2/vars.tf
@@ -0,0 +1,18 @@
+variable "PATH_TO_PRIVATE_KEY" {
+  default = "mykey"
+}
+
+variable "PATH_TO_PUBLIC_KEY" {
+  default = "mykey.pub"
+}
+
+variable "INSTANCE_USERNAME" {
+  default = "ubuntu"
+}
+
+variable "frontales" {
+  description = "Number of frontales servers"
+  type        = "list"
+  default     = ["Frontal-1", "Frontal-2"]  
+}
+
