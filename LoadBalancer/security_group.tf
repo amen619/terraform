0b931f29dbfe7c3f1fd8998f7b4e8d474144df28
Jon Udaondo <jonuda77@gmail.com>
2017-10-21 12:43:02 +0200

    ssh port added in security groups

--- LoadBalancer/security_group.tf
+++ LoadBalancer/security_group.tf
@@ -1,6 +1,7 @@
 resource "aws_security_group" "rule80" {
   name = "terraform_security_group"
 
+  # Access to port 80 apache
   ingress {
     from_port   = 80
     to_port     = 80
@@ -8,6 +9,15 @@ resource "aws_security_group" "rule80" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  # Access to port 22 ssh
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # Internet access
   egress {
     from_port   = 0
     to_port     = 0
